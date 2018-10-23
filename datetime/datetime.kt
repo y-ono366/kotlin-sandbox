0336f55ef5be08ff9f9dbef910c76ae01aa010e9
yusuke ohno <y.ono.varial@gmail.com>
2018-10-23 11:30:23 +0900

    日付の差分の計算

---
+++ datetime/datetime.kt
@@ -0,0 +1,26 @@
+import java.util.Calendar
+import java.text.SimpleDateFormat
+
+fun main(args: Array<String>) {
+  val beforeDate = "2018-07-01"
+  println(getDiffDay(beforeDate))
+}
+
+
+fun getDiffDay(beforeDate:String):Long {
+  val nowDate = Calendar.getInstance()
+  val calender = Calendar.getInstance()
+  val format = SimpleDateFormat("yyyy-MM-dd")
+  val date = format.parse(beforeDate)
+  calender.setTime(date)
+
+
+
+  println(nowDate.getTime())
+  println(calender.getTime())
+  val diffTime =  nowDate.getTimeInMillis() - calender.getTimeInMillis()
+  println(diffTime)
+  val millis_of_day = 1000 * 60 * 60 * 24
+  val diffDays = diffTime / millis_of_day
+  return diffDays
+}
